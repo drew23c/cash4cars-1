6637ad110c5fbd8c46de4dfbff2fe862e04e5554
drew23c <andrewcaldwell@ac.c4q.nyc>
2019-01-31 23:25:29 -0500

    added colors and animations to the website

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>CA$H FOR JUNK CARS, VAN & TRUCKS</h1>
+        <h1><strong className="cash">CA$H</strong> FOR JUNK CARS, VAN & TRUCKS</h1>
         <div className="towtruck-image">
           <img src={towtruck} style={{width:'60%', height:'60%'}} />
         </div>
@@ -17,16 +17,15 @@ class App extends Component {
           <h3>917-862-5466</h3>
         </div>
         <div className="features">
+          <h2>Services Included:</h2>
           <ul>
             <li><h2>Snow Removal</h2></li>
             <li><h2>Lost Papers, No problem</h2></li>
           </ul>
         </div>
-        <div className="contact-cash-image">
           <div className="cash-image">
             <img src={cash} style={{width:'30%', height:'30%'}} />
           </div>
-        </div>
       </div>
     );
   }
