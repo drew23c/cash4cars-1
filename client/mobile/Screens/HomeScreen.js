a16697e5e79409f8eb8173c010916cff06ff73c7
drew23c <andrewcaldwell@ac.c4q.nyc>
2019-02-05 17:26:35 -0500

    added footer and styling on the mobile side

--- client/mobile/Screens/HomeScreen.js
+++ client/mobile/Screens/HomeScreen.js
@@ -1,17 +1,20 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, Image, TouchableWithoutFeedback, ScrollView, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableWithoutFeedback, ScrollView, FlatList } from 'react-native';
 import Communications from 'react-native-communications';
-import {List} from 'react-native-elements';
 import towtruck from '../pics/towtruck.jpg';
 import towtruckIcon from '../pics/towtruckIcon.jpg';
 import cash from '../pics/cash.png';
 import phoneIcon from '../pics/phone.png';
+import phone from '../pics/baseline-phone-24px.svg';
 
 export default class App extends Component {
+  static navigationOptions = {
+    title:'TA Trucking'
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.title}>TA Trucking</Text>
+        {/* <Text style={styles.title}>TA Trucking</Text> */}
           <TouchableWithoutFeedback onPress={() => Communications.phonecall('9178625466', true)}><Text style={styles.phoneNumber}>Call 24/7 (917) 862-5466</Text></TouchableWithoutFeedback>
           <Image style={styles.image} source={towtruck} />
           <ScrollView>
@@ -30,21 +33,31 @@ export default class App extends Component {
           <View style={{alignItems:'center', position:'relative'}}>
             <Text style={{textAlign:'center', fontSize:20, paddingBottom:10}}>We buy junk cars, vans, trucks, SUVs, forklifts in all 5 boroughs</Text>
             <Text style={{fontSize:20, textAlign:'center'}}>Want to Junk your car, van truck SUV, forklift?</Text>
-                <TouchableWithoutFeedback onPress={() => Communications.phonecall('9178625466', true)}><Image source={phoneIcon} style={{height:80, width:80}} /></TouchableWithoutFeedback>
+                <TouchableWithoutFeedback onPress={() => Communications.phonecall('9178625466', true)}><Image source={phoneIcon} style={{height:80, width:80, borderRadius:25}} /></TouchableWithoutFeedback>
                 <Text style={{fontSize:20}}>Get a fast offer 24/7</Text>
-                <Image source={towtruckIcon} style={{height:80, width:80}} />
+                <Image source={towtruckIcon} style={{height:80, width:80, borderRadius:25}} />
                 <Text style={{fontSize:20, textAlign:'center'}}>Get a <Text style={{fontWeight:'bold', fontSize:20}}>FREE</Text> tow. Absolutely <Text style={{fontWeight:'bold'}}>FREE</Text> pickup included.</Text>
-                <Image source={cash} style={{height:80, width:80}} />
+                <Image source={cash} style={{height:80, width:80, borderRadius:25}} />
               <Text style={{fontSize:20, textAlign:'center'}}>Get paid immediately you get payment on the spot!</Text>
             <Text style={{fontSize:20, textAlign:'center'}}>Have any questions? <Text style={{fontSize:20}}>Call 24/7 (917) 862-5466</Text></Text>
             <Text style={{padding:5, fontSize:20}}>We buy junk cars for cash in the entire New York area. 
                 Sell your used or unwanted cars, trucks, SUVs, or vans and 
                 get paid on the spot. We don't just buy junk vehicles! We buy 
-                ANY condition vehicle with or without a tile or keys.</Text>
+                <Text style={{fontWeight:'bold'}}> ANY</Text> condition vehicle with or without a tile or keys.</Text>
             <Text style={{fontSize:20}}>Lost keys, <Text style={{fontWeight:'bold', fontSize:20}}>NO PROBLEM.</Text></Text>
           </View>
-
             </View>
+            <View className="footer">
+                <Text style={{textAlign:'center', paddingTop:10}}>TA Trucking</Text>
+                {/* <Text>Click to call <TouchableWithoutFeedback onPress={()=> Communications.phonecall("9178625466",true)}><Image source={phone} style={{height:50, width:50}} /></TouchableWithoutFeedback></Text> */}
+            </View>
+            {/* <FlatList
+              data={[
+                {key:"TA Trucking"},
+                {key:"Click to call", img: phone}
+              ]}
+              renderItem={({item})=> <View><Text>{item.key}</Text><TouchableWithoutFeedback onPress={()=> Communications.phonecall("9178625466")}><Image source={phone} style={{height:30, width:30}} /></TouchableWithoutFeedback></View>}
+            /> */}
         </ScrollView>
       </View>
     );
@@ -56,16 +69,16 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    // justifyContent: 'center',
+    justifyContent: 'center',
     width:'100%',
     height:'100%',
-    marginTop:0
+    top:0,
+    backgroundColor:'rgb(245, 240, 240)'
   },
   title:{
     position:'absolute',
     zIndex:10,
-    marginTop:30,
-    alignItems:'flex-start',
+    marginTop:0,
     fontSize:25,
     left:5
   },
