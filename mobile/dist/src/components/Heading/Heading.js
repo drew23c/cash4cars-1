f85a3cad23d0c1a59f251a941b6b5995f1ba00a8
drew23c <andrewcaldwell@ac.c4q.nyc>
2019-02-14 22:25:20 -0500

    re-adding files to mobile/dist

---
+++ mobile/dist/src/components/Heading/Heading.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './heading.css';
+
+export default class Heading extends Component{
+    render(){
+        return(
+            <div className="heading">
+                <h2><strong className="cash">CA$H</strong> FOR JUNK CARS, VANS & TRUCKS</h2>
+            </div>
+        )
+    }
+}
